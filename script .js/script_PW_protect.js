43d4e9fe0b6aee4aa33345f8e34ee8597741ca31
asep <adamdavidani@gmail.com>
2025-01-14 07:55:19 -0500

    Correcting changes from yesterday

---
+++ script .js/script_PW_protect.js
@@ -0,0 +1,28 @@
+
+//password = "2DAVIS";
+
+
+//inputPassword = "";
+
+//while (inputPassword != password) {
+    
+    
+    if (confirm("WARNING: This page is Password Protected")) {
+
+    }
+    else
+    {
+        window.location.href="index.html"
+    }
+   // if  (inputPassword == password) {
+       
+        //alert(" Password Correct, logging in!");
+        //break;
+   /* } else {
+        alert("Password incorrect, Please try again :(");
+       
+    }
+*///}
+//}
+    
+body.style.removeProperty("display");
