2efa6761fdd1cf34e8c669be633204e24480f68c
asep <adamdavidani@gmail.com>
2025-01-14 14:38:59 -0500

    Encryption Test & Others
    Part 1

---
+++ JS, CSS, & assets/script .js/Cypher.js
@@ -0,0 +1,46 @@
+function checkPass(code) {
+
+    let x = document.getElementById("fname")
+
+    let store = ""
+
+    for (let i = 0; i < x.value.length; i++) {
+        store += x.value.charCodeAt(i).toString()
+
+        while (store.length % 3 != 0) {
+            store = "0" + store;
+        }
+
+    }
+
+
+    if (code == store.toString()) {
+        return true
+    }
+    else {
+        false;
+    }
+}
+
+
+function setPass(input) {
+    
+
+    store = ""
+
+    for (i = 0; i < input.length; i++) {
+        console.log(input.charCodeAt(i).toString())
+        store += input.charCodeAt(i).toString()
+
+        while (store.length % 3 != 0) {
+            store = "0" + store;
+            console.log(store)
+        }
+
+    }
+
+
+    return store
+}
+
+console.log(setPass("Meep123"))
