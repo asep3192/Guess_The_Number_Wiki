129f33e82daf8b90d9cb884533b6af6aa024dc92
asep <adamdavidani@gmail.com>
2025-05-19 12:33:19 -0400

    Updates to 2DAVIS and Secret Page (GTN Stats)

--- script .js/modal_image_open_3.js
+++ script .js/modal_image_open_3.js
@@ -1,9 +1,9 @@
 // Get the modal
-var modal2 = document.getElementById("myModal2");
+var modal2 = document.getElementById("myModal3");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img2 = document.getElementById("myImg2");
-var modalImg2 = document.getElementById("img02");
+var img2 = document.getElementById("myImg3");
+var modalImg2 = document.getElementById("img03");
 //var captionText2 = document.getElementById("caption");
 img2.onclick = function(){
   modal2.style.display = "block";
@@ -12,13 +12,9 @@ img2.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close2")[0];
+var span2 = document.getElementsByClassName("close3")[0];
 
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
   modal2.style.display = "none";
 }
-
-
-
-
